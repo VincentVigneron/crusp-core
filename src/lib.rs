610909e03f5a98b1516ceca298ccc52c0f155266
Vincent Vigneron <vigneron.vincent.49@gmail.com>
2021-05-19 18:39:51 +0200

    Add Nullable and Mergeable traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 pub trait Subsumed {
     fn is_subsumed_under(&self, val: &Self) -> bool;
 }
+pub trait Nullable {
+    fn is_null(&self) -> bool;
+    fn null() -> Self;
+    fn nullify(&mut self) -> Self; // return previous value
+}
+pub trait Mergeable: Copy {
+    fn merge(&self, rhs: Self) -> Self;
+}
 
 #[macro_export]
 macro_rules! unwrap_first {
