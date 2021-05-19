0377402d6c1326e101ed582b144b9cbcb052c8d0
Vincent Vigneron <vigneron.vincent.49@gmail.com>
2021-05-19 15:39:57 +0200

    Add unwrap_first and unwrap_last macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 pub trait Subsumed {
     fn is_subsumed_under(&self, val: &Self) -> bool;
 }
+
+#[macro_export]
+macro_rules! unwrap_first {
+    ($vec:ident) => {
+        *$vec.first().unwrap()
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_last {
+    ($vec:ident) => {
+        *$vec.last().unwrap()
+    };
+}
